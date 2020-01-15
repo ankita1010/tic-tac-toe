764b4e0241f8ba633bab0ece7ae16205a7c790ce
Ankita Chakraborty <ankitachakraborty412@gmail.com>
2020-01-15 16:12:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import WebpackLogo from '../public/assets/webpack.png';
+import ReactLogo from '../public/assets/react.png';
+
+import './App.scss';
+
+const App = () => {
+	return (
+		<div className="app-block">
+			<h1>Welcome to Webpack configured React App</h1>
+			<h2>Make changes to App.js to start your development..</h2>
+			<div>
+				<img src={WebpackLogo} alt="logo for webpack"/>
+				<img src={ReactLogo} alt="logo for react js"/>
+			</div>
+		</div>
+	)
+};
+export default App;
