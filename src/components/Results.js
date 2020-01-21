a38045745c9665443c247c0dc02c7f2b87d646ab
Ankita Chakraborty <ankita_chakraborty@apple.com>
2020-01-21 14:49:04 +0530

    added styling for results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, Fragment } from 'react';
 import { StoreContext } from '../context';
 
 import './results.scss';
@@ -14,17 +14,23 @@ export const Results = () => {
 		<div className="results-wrapper">
 			<h2 className={winner ? "winner-header" : ""}>
 				{winner ?
-					`Woo hoo! ${winner.name} is the winner!`
-					: `Its a tie!!`
+					<Fragment>
+						<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="diploma" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="svg-inline--fa fa-diploma fa-w-20 fa-3x"><path fill="currentColor" d="M384 268.67V128H256v140.67l-95.16 164c-3.05 7.49 2.65 15.63 10.73 15.32l36.64.01 25.21 28.52c5.56 5.87 15.33 4.04 18.39-3.45L320 352l68.2 121.07c3.05 7.49 12.83 9.32 18.39 3.45l25.2-28.52 36.64-.01c8.08.31 13.78-7.83 10.73-15.32l-95.16-164zm-160-8.61V115.94c-9.64-2.45-19.21-5.19-28.65-8.37L68.39 64.78c-15.03-3.1-30.25 3.27-37.03 14.77-41.81 70.94-41.81 217.95 0 288.89 6.78 11.5 22.01 17.88 37.03 14.77l104.6-35.24L224 260.06zm384.64-180.5c-6.78-11.5-22.01-17.88-37.03-14.77l-126.96 42.78c-9.44 3.18-19.01 5.93-28.65 8.37v144.12l51.01 87.92 104.6 35.24c15.03 3.1 30.25-3.27 37.03-14.77 41.81-70.94 41.81-217.95 0-288.89z"></path></svg>
+						<span>{`Woo hoo! ${winner.name} is the winner!`}</span>
+					</Fragment>
+					: <Fragment>
+						<i className="far fa-meh"></i>
+						<span>{`Its a tie!`}</span>
+					</Fragment>
 				}
 			</h2>
-			<button className="theme-btn" onClick={handleRematch}>
+			<button className="theme-btn results-btn" onClick={handleRematch}>
 				Rematch
 				</button>
 			<p>
 				-- Or --
 				</p>
-			<button className="theme-btn" onClick={initState}>
+			<button className="theme-btn results-btn" onClick={initState}>
 				Clear and Go Back
 				</button>
 		</div>
