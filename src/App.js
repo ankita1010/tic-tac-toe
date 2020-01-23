8fbe6ab3fbd7150b7cc9ebdc1dc7ac0528c95e15
Ankita Chakraborty <ankita_chakraborty@apple.com>
2020-01-23 15:37:17 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const screens = {
 	STEP_CHOOSE_PLAYER: <ChoosePlayers />,
 	STEP_CHOOSE_SIDE: <ChooseSides />,
 	STEP_PLAY_GAME: <GameBox />,
-	STEP_SHOW_RESULTS: <Results/>
+	STEP_SHOW_RESULTS: <Results />
 };
 
 import './App.scss';
@@ -19,11 +19,23 @@ const App = () => {
 	const { state } = useContext(StoreContext);
 	const { currentStep } = state;
 
-	return (
-		<div className="main-block">
+	return [
+		<div className="main-block" key="main-block">
 			<h1>Tic Tac Toe</h1>
 			{screens[currentStep]}
-		</div>
-	)
+
+		</div>,
+		<footer className="footer" key="footer">
+			<span className="links">
+				<a href="https://github.com/ankita1010" target="_black" title="Github profile link">
+					<i className="fab fa-github" />
+				</a>
+				<a href="https://www.linkedin.com/in/ankita-chakraborty-98527b10a/" target="_black" title="Linkedin profile link">
+					<i className="fab fa-linkedin"/>
+				</a>
+			</span>
+			<span>Built in &nbsp;<i className="fab fa-react" /></span>
+		</footer>
+	]
 };
 export { App };
